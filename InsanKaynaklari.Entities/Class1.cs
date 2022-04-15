f233d33a97bfba1339c5d9928b1c9da3344985a2
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-04-15 15:09:40 +0300

    Entities başlıkarı eklendi. Interface eklenmesi gerekebilir.

--- InsanKaynaklari.Entities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace InsanKaynaklari.Entities
-{
-    public class Class1
-    {
-    }
-}
---
+++ InsanKaynaklari.Entities/Concrete/Leave.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklari.Entities.Concrete
+{
+    class Leave
+    {
+        //İzin Sınıfı
+    }
+}
