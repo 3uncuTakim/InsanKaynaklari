29b111cd656cc69c255cbb88730aee3a239b3d62
buse çevik <buse_cevik_67@hotmail.com>
2022-04-19 14:44:13 +0300

    migration tamamlandı

--- InsanKaynaklari.DataAccess/Migrations/20220419114312_Mapping.Designer.cs
+++ InsanKaynaklari.DataAccess/Migrations/20220419114312_Mapping.Designer.cs
@@ -4,14 +4,16 @@ using InsanKaynaklari.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InsanKaynaklari.DataAccess.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220419114312_Mapping")]
+    partial class Mapping
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
