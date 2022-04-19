abb002924eb8535f9575d299d13b927aa7a218ee
Mustafa Sungur TAS <sunqurtas@gmail.com>
2022-04-19 11:43:43 +0300

    PersonelMap and ShiftMap

--- InsanKaynaklari.DataAccess/Configuration/PersonelMAP.cs
+++ InsanKaynaklari.DataAccess/Configuration/PersonelMAP.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InsanKaynaklari.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,14 @@ using System.Threading.Tasks;
 
 namespace InsanKaynaklari.DataAccess.Configuration
 {
-    public class PersonelMAP
+    public class PersonelMAP : BaseMap<Personel>, IEntityTypeConfiguration<Personel>
     {
-        //Sungur
+        public void Configure(EntityTypeBuilder<Personel> builder)
+        {
+            builder.HasIndex(p => p.Email).IsUnique();
+            builder.Property(p => p.Activation).IsRequired();
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Password).HasMaxLength(50).IsRequired();
+        }
     }
 }
--- InsanKaynaklari.DataAccess/Configuration/ShiftMAP.cs
+++ InsanKaynaklari.DataAccess/Configuration/ShiftMAP.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InsanKaynaklari.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,14 @@ using System.Threading.Tasks;
 
 namespace InsanKaynaklari.DataAccess.Configuration
 {
-    public class ShiftMAP
+    public class ShiftMAP : BaseMap<Shift>, IEntityTypeConfiguration<Shift>
     {
-        //Sungur
+        public void Configure(EntityTypeBuilder<Shift> builder)
+        {
+            builder.Property(s => s.ShiftDay).IsRequired().HasDefaultValueSql("getdate()");
+            builder.Property(s => s.StartOfShift).IsRequired().HasDefaultValueSql("getdate()");
+            builder.Property(s => s.EndOfShift).IsRequired().HasDefaultValueSql("getdate()");
+            builder.Property(s => s.Description).IsRequired().HasMaxLength(500);
+        }
     }
 }
