41ebfc99f00187c65566e420b701c4aff8d32346
Azmi Daşman <azmidasman@gmail.com>
2022-04-20 13:51:25 +0300

    Calendar  Controller eklendi, dataaccess miras verildi, wwwroota dosyaları atıldı.

---
+++ InsanKaynaklari.UI/Controllers/CalendarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsanKaynaklari.UI.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
