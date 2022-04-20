6351c9d3ab7d9c693ebe68eb52f99cc497137455
Mustafa Sungur TAS <sunqurtas@gmail.com>
2022-04-20 13:44:03 +0300

    Mail Utilities

---
+++ InsanKaynaklari.UI/Mailing/FakeMailSender/FakeMailSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Utilities.Mailing.FakeMailSender
+{
+    public class FakeMailSender : IMailService
+    {
+        public void SendEmail(MailTemplate mailTemplate)
+        {
+            Console.WriteLine(mailTemplate);
+        }
+    }
+}
---
+++ InsanKaynaklari.UI/Mailing/MailTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlogProject.Utilities.Mailing
+{
+    public class MailTemplate
+    {
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string[] To { get; set; }
+
+        public MailTemplate(string from, string subject, string body, params string[] to)
+        {
+            From = from;
+            Subject = subject;
+            Body = body;
+            To = to;
+        }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
