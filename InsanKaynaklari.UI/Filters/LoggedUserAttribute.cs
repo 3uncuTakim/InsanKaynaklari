132de0e09979d670fd3319d137cd2090c0fa0e49
Mustafa Sungur TAS <sunqurtas@gmail.com>
2022-04-20 14:17:50 +0300

    Working Login

---
+++ InsanKaynaklari.UI/Filters/LoggedUserAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BlogProject.Filters
+{
+    public class LoggedUserAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string userId = context.HttpContext.Session.GetString("userId");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                string routePath = context.HttpContext.Request.Path;
+                context.Result = new RedirectToActionResult("Index", "LogIn", new { yonlen = routePath });
+            }
+        }
+    }
+}
