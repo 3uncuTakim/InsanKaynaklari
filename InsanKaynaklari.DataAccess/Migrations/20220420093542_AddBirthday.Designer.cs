81ee881d1220cd4d3e3213c5799d1ff21d58d985
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-04-20 12:37:09 +0300

    Resmi tatiller eklendi PersonelDeteails a Birthday eklendi

--- InsanKaynaklari.DataAccess/Migrations/20220420093542_AddBirthday.Designer.cs
+++ InsanKaynaklari.DataAccess/Migrations/20220420093542_AddBirthday.Designer.cs
@@ -4,14 +4,16 @@ using InsanKaynaklari.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InsanKaynaklari.DataAccess.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220420093542_AddBirthday")]
+    partial class AddBirthday
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
