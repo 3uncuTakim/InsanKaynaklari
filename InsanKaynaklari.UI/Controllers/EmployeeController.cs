7c25b184f8e16247d32d752cecd803a92f3bc124
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-04-20 11:57:59 +0300

    viewmodel

---
+++ InsanKaynaklari.UI/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using InsanKaynaklari.UI.API;
+using InsanKaynaklari.UI.ViewModels.Employee;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklari.UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            string json = new WebClient().DownloadString("https://api.ubilisim.com/resmitatiller/");
+            PublicHolidayRoot publicHoliday = JsonConvert.DeserializeObject<PublicHolidayRoot>(json);
+            EmployeeMainPageVM emp = new EmployeeMainPageVM
+            {
+                PublicHoliday = publicHoliday
+            };
+            return View(emp);
+        }
+    }
+}
---
+++ InsanKaynaklari.UI/ViewModels/Employee/EmployeeMainPageVM.cs
@@ -0,0 +1,13 @@
+﻿using InsanKaynaklari.UI.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklari.UI.ViewModels.Employee
+{
+    public class EmployeeMainPageVM
+    {
+        public PublicHolidayRoot PublicHoliday { get; set; }
+    }
+}
