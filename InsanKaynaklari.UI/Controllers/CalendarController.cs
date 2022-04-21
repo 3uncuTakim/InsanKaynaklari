fb55ab1915b5a09a99b76f6c1cf9d82f273f1298
buse çevik <buse_cevik_67@hotmail.com>
2022-04-21 14:10:30 +0300

    ..

--- InsanKaynaklari.UI/Controllers/CalendarController.cs
+++ InsanKaynaklari.UI/Controllers/CalendarController.cs
@@ -1,18 +1,27 @@
-﻿using InsanKaynaklari.UI.Filters;
+﻿using InsanKaynaklari.DataAccess.Context;
+using InsanKaynaklari.UI.Filters;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace InsanKaynaklari.UI.Controllers
 {
     [LoggedUser]
     public class CalendarController : Controller
     {
+
+        private readonly DatabaseContext _context;
+        public CalendarController(DatabaseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult GetAllEvent()
         {
-            DatabaseContext db = new DatabaseContext();
+            var Event = _context.Events.ToList();
+            return new JsonResult(Event);
         }
     }
 }
