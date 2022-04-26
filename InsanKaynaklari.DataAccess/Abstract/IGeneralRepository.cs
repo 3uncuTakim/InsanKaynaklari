e8ca0458b8efbaf6cb2cd73df2914eb2b2cc6654
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-04-26 15:50:56 +0300

    General repository giriş

---
+++ InsanKaynaklari.DataAccess/Abstract/IGeneralRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsanKaynaklari.DataAccess.Abstract
+{
+    public interface IGeneralRepository<T>where T:class
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(object id);
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
