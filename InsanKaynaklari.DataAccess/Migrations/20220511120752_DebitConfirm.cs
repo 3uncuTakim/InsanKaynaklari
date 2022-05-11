c925e9d2522fc40b2906594d832c87357a63336c
Tolunay Gençman <tolunaygencman@hotmail.com>
2022-05-11 15:08:46 +0300

    Debit confirm eklendi.

---
+++ InsanKaynaklari.DataAccess/Migrations/20220511120752_DebitConfirm.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InsanKaynaklari.DataAccess.Migrations
+{
+    public partial class DebitConfirm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCorfirmed",
+                table: "Debits",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCorfirmed",
+                table: "Debits");
+        }
+    }
+}
