e26dd50582272ed253bbcc554675fb79ad29040d
sunqur <sunqurtas@gmail.com>
2022-05-12 10:32:12 +0300

    Debit işlemleri

--- InsanKaynaklari.UI/Controllers/DebitController.cs
+++ InsanKaynaklari.UI/Controllers/DebitController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsanKaynaklari.DataAccess.Context;
+using InsanKaynaklari.UI.Filters;
+using InsanKaynaklari.UI.ViewModels.Debit;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,33 @@ using System.Threading.Tasks;
 
 namespace InsanKaynaklari.UI.Controllers
 {
+    [LoggedUser]
     public class DebitController : Controller
     {
-        public IActionResult Index()
+        private readonly DatabaseContext _context;
+
+        public DebitController(DatabaseContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpGet("[controller]/[action]/{Id}")]
+        public IActionResult Index(string id)
+        {
+            var debit = (from deb in _context.Debits
+                         where deb.PersonelID == Convert.ToInt32(id)
+                         select new DebitViewModel
+                         {
+                             DebitName=deb.DebitName,
+                             DebitCode=deb.DebitCode,
+                             DateOfIssue=deb.DateOfIssue,
+                             Description=deb.Description,
+                             DateOfReturn=deb.DateOfReturn
+                         }
+                         ).ToList();
+           
+           
+            return View(debit);
             //izinlerdekinin aynısı webgrid kulllanılacak (sb admin-2)
             //Onay butonu olacak (edit işlemi sadece confirm status ü değiştirecek)!sadece bir kere editleyebilecek
             //İtiraz butonu olacak(e-mail (form şeklinde))
---
+++ InsanKaynaklari.UI/ViewModels/Debit/DebitViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace InsanKaynaklari.UI.ViewModels.Debit
+{
+    public class DebitViewModel
+    {
+        [Required]
+        public string DebitName { get; set; }
+        [Required]
+        public string DebitCode { get; set; }
+        [Required]
+        public DateTime DateOfIssue { get; set; }
+        public DateTime? DateOfReturn { get; set; }
+        [Required]
+        public string Description { get; set; }
+        
+    }
+}
